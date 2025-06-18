ec02cf3fe0f6bb78fa7bfe17be732ae3a4738a72
azeezfasasi <fas.azeez@gmail.com>
2025-06-18 06:03:20 +0100

    checks

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,7 +1,16 @@
+// const cloudinary = require('cloudinary').v2;
+
+// cloudinary.config({
+//   cloud_name: 'dtvyv2qvi',
+//   api_key: '264143534149483',
+//   api_secret: 'mNf0yXlcQryEY8yPgbe1GNMhOhc'
+// });
+
 const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
 
 cloudinary.config({
-  cloud_name: 'dtvyv2qvi',
-  api_key: '264143534149483',
-  api_secret: 'mNf0yXlcQryEY8yPgbe1GNMhOhc'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
